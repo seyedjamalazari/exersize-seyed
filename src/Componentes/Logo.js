48c2a49318a5ab1a28d37badee8300fe249285f5
seyed <seedjamal2@gmail.com>
2022-04-05 15:00:08 +0430

    seyed-jamal

---
+++ src/Componentes/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <h1 className='app'>app</h1>
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/Componentes/Mpxl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mpxl = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Mpxl;
+            
